37c4479ba08681f5df3aa86cf726af8a461a4931
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-08-01 11:38:57 +0200

    override compiler to clang++

--- build.rs
+++ build.rs
@@ -4,9 +4,11 @@ fn main() {
     //panic!("{:?}", std::env::current_dir().unwrap());
     cc::Build::new()
         .cuda(true)
-	.flag("--cubin")
-	.flag("-allow-unsupported-compiler")
+        .flag("--cubin")
+        .flag("-cudart=shared")
         .file("src/kernels/sin.cu")
+        .ccbin(false)
+        .flag("-ccbin=clang++")
         .compile("libsin.cubin");
 
     /* Link CUDA Runtime (libcudart.so) */
