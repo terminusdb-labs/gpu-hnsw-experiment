ec3ecdcfb52abe9da5468907fd95d52f6da2a08c
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-08-01 12:49:07 +0200

    working build on nixos

--- hnsw-kernels/build.rs
+++ hnsw-kernels/build.rs
@@ -1,11 +1,11 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/compatibility.cuh");
-    println!("cargo:rerun-if-changed=src/cuda_utils.cuh");
-    println!("cargo:rerun-if-changed=src/binary_op_macros.cuh");
+    println!("cargo:rerun-if-changed=src/*.cu");
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
 
     let builder = bindgen_cuda::Builder::default();
-    println!("cargo:info={builder:?}");
+    //println!("cargo:info={builder:?}");
     let bindings = builder.build_ptx().unwrap();
-    bindings.write("src/lib.rs").unwrap();
+    bindings.write(format!("{out_dir}/kernels.rs")).unwrap();
 }
--- shell.nix
+++ shell.nix
@@ -15,8 +15,10 @@ pkgs.mkShell {
   shellHook = ''
 LD_LIBRARY_PATH=${pkgs.cudatoolkit}/lib:$LD_LIBRARY_PATH
 if [ -e "/etc/NIXOS" ];then
-  LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudatoolkit}/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudatoolkit}/lib:$LD_LIBRARY_PATH
 fi
+export CUDA_PATH=${pkgs.cudatoolkit}
+export NVCC_CCBIN=${pkgs.clang_15}/bin/clang++
    '';
 
 }
